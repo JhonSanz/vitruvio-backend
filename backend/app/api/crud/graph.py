db84747ba635b0465c95e9e6da0bc0a6b9f62cd9
JhonSanz <ingjhonsanz@gmail.com>
2024-08-05 16:29:11 -0500

    Added nodes query

--- backend/app/api/crud/graph.py
+++ backend/app/api/crud/graph.py
@@ -4,6 +4,7 @@ from backend.app.api.crud.relation import create_relation_graph
 from backend.app.api.schemas.graph import DataModel, DataInsumos
 from backend.app.api.schemas.item import ItemCreate
 from backend.app.api.schemas.relation import RelationCreateInsumo
+from backend.app.api.utils.node_format import extract_node_properties
 
 
 def create_graph(data_model: DataModel):
@@ -32,3 +33,20 @@ def create_insumo(data_model: DataInsumos):
             target_code=rel.related,
             properties=rel.params
         ))
+
+
+def show_nodes(*, direction: str):
+    query = (
+        "MATCH (n) "
+        "WHERE NOT (n)-->() "
+        "RETURN n "
+    )
+
+    try:
+        result, _ = db.cypher_query(query)
+        nodes = [extract_node_properties(record[0]) for record in result]
+        return nodes
+    except Exception as e:
+        print(f"Failed to fetch nodes: {str(e)}")
+        return []
+
--- backend/app/api/routes/graph.py
+++ backend/app/api/routes/graph.py
@@ -1,8 +1,8 @@
 from typing import List, Optional
 from fastapi import APIRouter, HTTPException
-from backend.app.api.crud import graph as create_graph
+from backend.app.api.crud import graph as crud_graph
 from backend.app.api.schemas.graph import DataModel, DataInsumos
-
+from backend.app.api.crud.item import Item
 
 
 router = APIRouter()
@@ -11,16 +11,22 @@ router = APIRouter()
 @router.post("/")
 def receive_data(data_model: DataModel):
     try:
-        create_graph.create_graph(data_model)
+        crud_graph.create_graph(data_model)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to create graph {e}")
+
+
+@router.get("/", response_model=List[Item])
+def get_items(direction: str | None = None):
+    items = crud_graph.show_nodes(direction="")
+    return items
 
 
 @router.post("/insumo")
 def create_insumo(data_model: DataInsumos):
     print("route", data_model)
     try:
-        create_graph.create_insumo(data_model)
+        crud_graph.create_insumo(data_model)
         return 200
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Failed to create graph {e}")
